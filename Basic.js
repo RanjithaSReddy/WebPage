21f001143befa604d512f82c3572012b0635b25e
RanjithaSReddy <RanjithaSReddy>
2021-05-09 12:30:02 +0530

    commit first

---
+++ Basic.js
@@ -0,0 +1,42 @@
+console.log("hi");
+// //Program of array, reverse, uppercase, endswith
+// countries=["India", "USA", "koria", "Switzerland"]
+//  Newcountries=[]
+//  result=[]
+//   for(let i=countries.length-1; i>=0; i--){
+//     Newcountries.push(countries[i])
+//     result.push(countries[i].toUpperCase())
+//  }
+// console.log(Newcountries)
+// console.log(result)
+
+// console.log("end of program1")
+
+// console.log("Program2")
+
+// // let value=[2,3,5,6]
+// let arrfunc= (...values) => 
+// { let Newresult=1
+//     for(let item of values){
+//      Newresult *= item
+//     }
+//     return Newresult
+// }
+// arrfunc(2,4,8)
+
+let fibfun = (...val) =>{
+    let n1=0;
+    let n2=1;
+    let n3
+    console.log(n1)
+    for()
+
+}
+
+
+
+
+
+
+
+
